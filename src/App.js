ef5563d6928ca247f46ba51a94c8237919d3c9b2
Horlamidey <abdulrahmanbeyioku@gmail.com>
2022-09-28 17:04:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Product from './Pages/Product';
+import Contact from './Pages/Contact';
+import Cart from './Pages/Cart';
+import FAQ from './Pages/FAQ';
 import './index.css';
-import FunctionalComp from './components/FunctionalComp';
-import ClassComp from './components/ClassComp';
+
 
 export class App extends React.Component {
   render() {
     return (
       <div> 
-        <Route></Route>
-        <h1>Hello World......</h1>
-        <FunctionalComp/>
-        <ClassComp/>
+        <Route path='/'>
+          <Home/>
+        </Route>
+        <Route path='/product'>
+          <Product />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+        <Route path='/FAQ'>
+          <FAQ />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/login'>
+          <Login/>
+        </Route>
       </div>
     )
   }
--- src/components/FunctionalComp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FunctionalComp() {
-  return (
-    <div>FunctionalComp</div>
-  )
-}
